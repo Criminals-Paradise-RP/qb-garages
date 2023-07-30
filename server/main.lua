3691a2fc724b479dfe9df0e6fd33279639064aed
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-07-30 13:27:02 -0700

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -263,7 +263,13 @@ QBCore.Functions.CreateCallback("qb-garage:server:checkOwnership", function(sour
     elseif garageType == "house" then     --House garages only for player cars that have keys of the house
          MySQL.query('SELECT * FROM player_vehicles WHERE plate = ?', {plate}, function(result)
             if result[1] then
-                cb(true)
+                local hasHouseKey = exports['ps-housing']:IsOwner(src, house) -- ps-housing
+                -- local hasHouseKey = exports['qb-houses']:hasKey(result[1].license, result[1].citizenid, house)
+                if hasHouseKey then
+                    cb(true)
+                else
+                    cb(false)
+                end
             else
                 cb(false)
             end
